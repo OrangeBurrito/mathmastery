a3eb47170a8772f360888578b8ccc5d21ba883da
orangeburrito <mali.kewl.pool@gmail.com>
2020-04-03 15:56:55 +0700

    Co-authored-by: Paul Krisadaolrn <pauly.k@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,4 +1,40 @@
-import { genSubtractionQuestions } from './generator.js'
-console.clear();
 
-console.log(genSubtractionQuestions)
+
+document.body.appendChild(createHeader())
+document.body.appendChild(createProblemSet(['3 * 2', '3 * 4', '3 * 5', '3 * 6', '3 * 7']))
+
+
+function createHeader() {
+	const header = document.createElement('header')
+	header.textContent = 'Math Mastery'
+	return header
+}
+
+
+function createProblemSet(expressions) {
+	const problemSet = document.createElement('section')
+	problemSet.classList.add('problem-set')
+
+	expressions.forEach(e => problemSet.appendChild(createQuestion(e)))
+
+	return problemSet
+}
+
+
+function createQuestion(expression) {
+	const q = document.createElement('div')
+	q.className = 'question'
+	
+	const label = document.createElement('label')
+	label.textContent = expression
+	
+	const input = document.createElement('input')
+	input.setAttribute('text', 'number')
+	
+	q.appendChild(label)
+	q.appendChild(input)
+
+	return q
+}
+
+
--- src/generator.js
+++ src/generator.js
@@ -9,3 +9,15 @@ export function genAdditionQuestions(min, max, count) {
 export function genSubtractionQuestions(min, max, count) {
 	return ['30 - 14', '65 - 12']
 }
+
+export function initialQuestions(){
+	return [
+		{expression: '2 * 2', answer: 4},
+		{expression: '2 * 3', answer: 6},
+		{expression: '2 * 4', answer: 8},
+		{expression: '2 * 5', answer: 10},
+		{expression: '2 * 6', answer: 12},
+		{expression: '2 * 7', answer: 14},
+		{expression: '2 * 8', answer: 16}
+	]
+}
