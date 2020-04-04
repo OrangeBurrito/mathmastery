faf79b46a9bb9e19d87169c0076da504d8ede313
orangeburrito <mali.kewl.pool@gmail.com>
2020-04-04 18:13:58 +0700

    moved problem sets out of render

--- src/app.js
+++ src/app.js
@@ -1,8 +1,32 @@
-render()
 
-function render() {
-	document.body.appendChild(Header('Math Mastery'))
-	document.body.appendChild(ProblemSet(['3 * 2', '3 * 4', '3 * 5', '3 * 6', '3 * 7']))
+const problemSets = [
+	{
+		name: '2 times tables',
+		expressions: [
+			{ label: '2 * 2', answer: 4 },
+			{ label: '2 * 4', answer: 8 }, 
+			{ label: '2 * 5', answer: 10 }, 
+			{ label: '2 * 6', answer: 12 }, 
+			{ label: '2 * 7', answer: 14 }
+		]		
+	},
+	{
+		name: '3 times tables',
+		expressions: [
+			{ label: '3 * 2', answer: 6 },
+			{ label: '3 * 4', answer: 12 }, 
+			{ label: '3 * 5', answer: 15 }, 
+			{ label: '3 * 6', answer: 18 }, 
+			{ label: '3 * 7', answer: 21 }
+		]		
+	},
+]
+
+render(problemSets[1])
+
+function render(problemSet) {
+	document.body.appendChild(Header('Math Mastery ' + problemSet.name))
+  document.body.appendChild(ProblemSet(problemSet.expressions))
 }
 
 function Header(title) {
@@ -16,22 +40,27 @@ function ProblemSet(expressions) {
 	const problemSet = document.createElement('section')
 	problemSet.classList.add('problem-set')
 
-	expressions.forEach(expression => problemSet.appendChild(Question(expression)))
+	expressions.forEach(expression => { 
+		let questionComponent = Question(expression)
+		problemSet.appendChild(questionComponent)
+	})
 
 	return problemSet
 }
 
 
 function Question(expression) {
+
 	const q = document.createElement('div')
 	q.className = 'question'
-	
+	q.setAttribute('answer', expression.answer)
+
 	const label = document.createElement('label')
-	label.textContent = expression
-	
+	label.textContent = expression.label
+
 	const input = document.createElement('input')
 	input.setAttribute('text', 'number')
-	
+
 	q.appendChild(label)
 	q.appendChild(input)
 
